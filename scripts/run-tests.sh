375fa8a380d79b6143daba75266a5da5e9e6ea48
Jingtao Xu <jingtao@gmail.com>
2021-10-11 07:26:49 +0800

    update scripts for github actions

---
+++ scripts/run-tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e # exit on any failure
+ros run -- --version
+ros run -e '(pushnew :test *features*)' \
+    -e '(ql:quickload :literate-lisp)' \
+    -e '(literate-lisp:with-literate-syntax (load "./literate-lisp.org"))' \
+    -e '(ql:quickload :literate-demo)' \
+    -e '(format t "Run test in ~A ~A~%" (lisp-implementation-type) (lisp-implementation-version))' \
+    -e '(if (not (literate-lisp::run-test)) (uiop:quit 1))' \
+    -e '(if (not (islands-puzzle::run-test)) (uiop:quit 1))' \
+    -e '(if (not (literate-demo::run-test)) (uiop:quit 1))' \
+    -e '(uiop:quit 0)'
